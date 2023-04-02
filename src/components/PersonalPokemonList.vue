50c09a9bdff2e2e4015eaf6beacc49e5ef66dc77
liamthys <liam.thys11@gmail.com>
2023-04-02 18:35:08 +0200

    feat: list items on homepage

---
+++ src/components/PersonalPokemonList.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import IconPokeball from './icons/IconPokeball.vue'
+
+defineProps<{
+  listName: string
+  listType: string
+}>()
+</script>
+
+<template>
+  <div class="item" :style="{ background: `var(--gradient-${listType})` }">
+    <h2>{{ listName }}</h2>
+    <h3>4 pokemon</h3>
+    <i>
+      <IconPokeball />
+    </i>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  padding: 15% 2rem;
+
+  /* Purple Gradient */
+
+  border-radius: 10px;
+}
+
+h2 {
+  font-family: 'SF Pro Text';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 22px;
+  /* identical to box height, or 122% */
+
+  letter-spacing: -0.408px;
+
+  color: var(--vt-c-white);
+
+  position: relative;
+  top: 1rem;
+  right: 1.2rem;
+}
+
+h3 {
+  font-family: 'SF Pro Text';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 22px;
+  /* identical to box height, or 147% */
+
+  letter-spacing: -0.408px;
+
+  color: rgba(255, 255, 255, 0.5);
+
+  position: relative;
+  top: 1rem;
+  right: 1.2rem;
+}
+
+i {
+  position: absolute;
+  top: 6px;
+  right: 0px;
+  width: 45%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,33 @@
 import PokemonSearch from '@/components/PokemonSearch.vue'
 import PokemonList from '@/components/PokemonList.vue'
 import MainTitle from '@/components/MainTitle.vue'
+import PersonalPokemonList from '@/components/PersonalPokemonList.vue'
 
 import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <main>
-    <MainTitle msg="Pokedex" />
-    <nav>
-      <RouterLink to="/team">Mijn team</RouterLink>
-      <RouterLink to="/favorites">Favorieten</RouterLink>
-    </nav>
+    <MainTitle msg="Pokédex" />
     <PokemonSearch />
+    <nav>
+      <RouterLink to="/team"
+        ><PersonalPokemonList listName="Mijn team" listType="team"
+      /></RouterLink>
+      <RouterLink to="/favorites"
+        ><PersonalPokemonList listName="Favorieten" listType="favorites"
+      /></RouterLink>
+    </nav>
     <PokemonList />
   </main>
 </template>
+
+<style scoped>
+nav {
+  margin: 10px 0;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10px;
+}
+</style>
